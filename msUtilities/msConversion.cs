6ceb2df0acece85d566b36ca95550a09f1a4c057
Phelipe de Sterlich <p.desterlich@gmail.com>
2016-01-19 10:03:08 +0100

    added msMonth, msYear and msDateHelpers classes * msMonth: handle a msMonth object (startDate, endDate, list<msMonth>) * msYear: handle a msYear object (startDate, endDate, list<msYear>) * msDateHelpers: helpers functions to manage dates

--- msUtilities/msConversion.cs
+++ msUtilities/msConversion.cs
@@ -4,13 +4,11 @@ namespace msUtilities
 {
   public class msConversion
   {
-
-    /**
-     * converts a c# DateTime object to a Unix TimeStamp
-     *
-     * @param  DateTime sourceDate datetime ojbect to convert
-     * @output double              unit timestamp representingthe original datetime object
-     **/
+    /// <summary>
+    /// converts a c# DateTime object to a Unix TimeStamp
+    /// </summary>
+    /// <param name="sourceDate">datetime object to convert</param>
+    /// <returns>unix timestamp representing the original datetime object</returns>
     public static double DateTimeToUnixTimestamp(DateTime sourceDate)
     {
       return Math.Truncate((sourceDate - new DateTime(1970, 1, 1).ToLocalTime()).TotalMilliseconds);
