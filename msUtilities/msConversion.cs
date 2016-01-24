8616f8dbefe3a6bb278c74fb8d2976b56db8e47e
Phelipe de Sterlich <p.desterlich@gmail.com>
2016-01-24 17:39:00 +0100

    added msConversion functions stringToInt and stringToFloat

--- msUtilities/msConversion.cs
+++ msUtilities/msConversion.cs
@@ -14,5 +14,34 @@ namespace msUtilities
       return Math.Truncate((sourceDate - new DateTime(1970, 1, 1).ToLocalTime()).TotalMilliseconds);
     }
 
+    /// <summary>
+    /// converts a string into an int value, with a default in case of errors
+    /// </summary>
+    /// <param name="value">string value to convert</param>
+    /// <param name="defaultValue">default value</param>
+    /// <returns>int converted value</returns>
+    public static int stringToInt(String value, int defaultValue)
+    {
+      int j;
+      if (int.TryParse(value, out j))
+        return j;
+      else
+        return defaultValue;
+    }
+
+    /// <summary>
+    /// converts a string into a float value, with a default in case of errors
+    /// </summary>
+    /// <param name="value">string value to convert</param>
+    /// <param name="defaultValue">default value</param>
+    /// <returns>float converted value</returns>
+    public static float stringToFloat(String value, float defaultValue)
+    {
+      float j;
+      if (float.TryParse(value, out j))
+        return j;
+      else
+        return defaultValue;
+    }
   }
 }
