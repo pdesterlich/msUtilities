18c0894d405a757b66152878b3f3c083af719ff4
Phelipe de Sterlich <p.desterlich@gmail.com>
2016-01-31 14:41:55 +0100

    added functions to msXmlHelpers to extract info from a xml node

--- msUtilities/msXmlHelpers.cs
+++ msUtilities/msXmlHelpers.cs
@@ -6,7 +6,7 @@ namespace msUtilities
   public class msXmlHelpers
   {
     /// <summary>
-    /// returns the value (string) of a single node
+    /// returns the value (string) of a single node (inside a xml document)
     /// </summary>
     /// <param name="xml">xml document</param>
     /// <param name="path">node path</param>
@@ -23,7 +23,24 @@ namespace msUtilities
     }
 
     /// <summary>
-    /// returns the value (int) of a single node
+    /// returns the value (string) of a single node (inside a xml node)
+    /// </summary>
+    /// <param name="xml">xml node</param>
+    /// <param name="path">node path</param>
+    /// <param name="defaultValue">default value if not found</param>
+    /// <returns>node value</returns>
+    public static String get(XmlNode xml, String path, String defaultValue)
+    {
+      XmlNode node = xml.SelectSingleNode(path);
+
+      if (node == null)
+        return defaultValue;
+      else
+        return node.InnerText;
+    }
+
+    /// <summary>
+    /// returns the value (int) of a single node (inside a xml document)
     /// </summary>
     /// <param name="xml">xml document</param>
     /// <param name="path">node path</param>
@@ -47,13 +64,61 @@ namespace msUtilities
     }
 
     /// <summary>
-    /// returns the value (bool) of a single node
+    /// returns the value (int) of a single node (inside a xml node)
+    /// </summary>
+    /// <param name="xml">xml node</param>
+    /// <param name="path">node path</param>
+    /// <param name="defaultValue">default value if not found or in case of conversion errors</param>
+    /// <returns>node value</returns>
+    public static int get(XmlNode xml, String path, int defaultValue)
+    {
+      XmlNode node = xml.SelectSingleNode(path);
+
+      if (node == null)
+        return defaultValue;
+      else
+      {
+        int j;
+        String value = xml.SelectSingleNode(path).InnerText;
+        if (Int32.TryParse(value, out j))
+          return j;
+        else
+          return defaultValue;
+      }
+    }
+
+    /// <summary>
+    /// returns the value (bool) of a single node (inside a xml document)
     /// </summary>
     /// <param name="xml">xml document</param>
     /// <param name="path">node path</param>
     /// <param name="defaultValue">default value if not found or in case of conversion errors</param>
     /// <returns>node value</returns>
     public static bool get(XmlDocument xml, String path, bool defaultValue)
+    {
+      XmlNode node = xml.SelectSingleNode(path);
+
+      if (node == null)
+        return defaultValue;
+      else
+      {
+        bool j;
+        String value = xml.SelectSingleNode(path).InnerText;
+        if (bool.TryParse(value, out j))
+          return j;
+        else
+          return defaultValue;
+      }
+    }
+
+    /// <summary>
+    /// returns the value (bool) of a single node (inside a xml node)
+    /// </summary>
+    /// <param name="xml">xml node</param>
+    /// <param name="path">node path</param>
+    /// <param name="defaultValue">default value if not found or in case of conversion errors</param>
+    /// <returns>node value</returns>
+    public static bool get(XmlNode xml, String path, bool defaultValue)
     {
       XmlNode node = xml.SelectSingleNode(path);
 
