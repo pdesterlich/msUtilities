ad0e020dfede3b02848583244764f227a3dafcb0
Phelipe de Sterlich <p.desterlich@gmail.com>
2016-03-04 22:03:16 +0100

    added msUtilities class with getAppVersion function

---
+++ msUtilities/msUtilities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace msUtilities
+{
+  public class msUtilities
+  {
+    /// <summary>
+    /// returns current application version
+    /// </summary>
+    /// <param name="fieldCount">number of fields to return - default 3 (major.minor.build)</param>
+    /// <returns>string</returns>
+    public static string getAppVersion(int fieldCount = 3)
+    {
+      Version version = Assembly.GetExecutingAssembly().GetName().Version;
+      return version.ToString(fieldCount);
+    }
+  }
+}
