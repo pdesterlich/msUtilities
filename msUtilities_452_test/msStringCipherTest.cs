d69569c20cb0b22282d1eb04e374f8296be7c5ec
Phelipe de Sterlich <p.desterlich@gmail.com>
2016-03-05 22:30:47 +0100

    added test project for msUtilities (dot net 4.5.2)

---
+++ msUtilities_452_test/msStringCipherTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using msUtilities;
+
+namespace msUtilities_452_test
+{
+  [TestClass]
+  public class msStringCipherTest
+  {
+    [TestMethod]
+    public void TestStringEncryptDecrypt()
+    {
+      string testo = "this is a text";
+      string password = "password";
+
+      string encrypted = msStringCipher.Encrypt(testo, password);
+      string decrypted = msStringCipher.Decrypt(encrypted, password);
+
+      Assert.AreEqual(testo, decrypted);
+    }
+
+    [TestMethod]
+    public void TestStringEncryptDecryptWithDifferentText()
+    {
+      string testo = "this is a different (and longer) text";
+      string password = "this is a password";
+
+      string encrypted = msStringCipher.Encrypt(testo, password);
+      string decrypted = msStringCipher.Decrypt(encrypted, password);
+
+      Assert.AreEqual(testo, decrypted);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(System.Security.Cryptography.CryptographicException))]
+    public void TestStringEncryptDecryptWithWrongPassword()
+    {
+      string testo = "this is a text";
+      string password = "password";
+      string wrongPassword = "wrong_password";
+
+      string encrypted = msStringCipher.Encrypt(testo, password);
+      string decrypted = msStringCipher.Decrypt(encrypted, wrongPassword);
+    }
+  }
+}
