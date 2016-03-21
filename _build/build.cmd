29fc28caff76d5488a3ea5f5f175ace3aa01ff64
Phelipe de Sterlich <p.desterlich@gmail.com>
2016-03-21 09:06:49 +0100

    added simple build script

---
+++ _build/build.cmd
@@ -0,0 +1,15 @@
+set pathBase=%CD%\..
+
+call localvars.cmd
+
+call "%pathMsBuildInit%"
+
+cd %pathBase%
+
+rem eseguo una pulizia delle directory
+msbuild /t:clean /p:configuration=Release
+
+rem compilo i due progetti
+msbuild /t:build /p:configuration=Release
+
+cd %pathBase%\_build
