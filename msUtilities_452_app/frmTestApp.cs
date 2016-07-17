330008028375013b45a85f72ea2e394187466048
Phelipe de Sterlich <p.desterlich@gmail.com>
2016-07-18 01:19:27 +0200

    added project msUtilities.Database.Forms.Wpf.452
    added new wpf library to hold custom forms for database needs

--- msUtilities_452_app/frmTestApp.cs
+++ msUtilities_452_app/frmTestApp.cs
@@ -1,91 +1,91 @@
-﻿using System;
-using System.Windows.Forms;
-using msUtilities;
+﻿using msUtilities;
 using msUtilities.Database;
+using System;
+using System.Windows.Forms;
 
 namespace msUtilities_452_app
 {
-  public partial class frmTestApp : Form
-  {
-    public frmTestApp()
+    public partial class frmTestApp : Form
     {
-      InitializeComponent();
-    }
+        public frmTestApp()
+        {
+            InitializeComponent();
+        }
 
-    private void btnTestMsConnectionForms_Click(object sender, EventArgs e)
-    {
-      var connectionForm = new msConnectionForm();
-      var connectionParams = new msConnectionParams();
-      if (connectionForm.showDialog("msUtilities Test App", ref connectionParams))
-      {
-        MessageBox.Show(connectionParams.ToString());
-        MessageBox.Show(connectionParams.getConnectionString());
-      }
-    }
+        private void btnTestMsConnectionForms_Click(object sender, EventArgs e)
+        {
+            var connectionForm = new msConnectionForm();
+            var connectionParams = new msConnectionParams();
+            if (connectionForm.showDialog("msUtilities Test App", ref connectionParams))
+            {
+                MessageBox.Show(connectionParams.ToString());
+                MessageBox.Show(connectionParams.getConnectionString());
+            }
+        }
 
-    private void btnTestMsConnectionFormsSqlServer_Click(object sender, EventArgs e)
-    {
-      var connectionForm = new msConnectionForm();
-      var connectionParams = new msConnectionParams();
-      connectionParams.databaseType = DatabaseType.SqlServer;
-      connectionParams.database = "database";
-      if (connectionForm.showDialog("msUtilities Test App", ref connectionParams))
-      {
-        MessageBox.Show(connectionParams.ToString());
-        MessageBox.Show(connectionParams.getConnectionString());
-      }
-    }
+        private void btnTestMsConnectionFormsSqlServer_Click(object sender, EventArgs e)
+        {
+            var connectionForm = new msConnectionForm();
+            var connectionParams = new msConnectionParams();
+            connectionParams.databaseType = DatabaseType.SqlServer;
+            connectionParams.database = "database";
+            if (connectionForm.showDialog("msUtilities Test App", ref connectionParams))
+            {
+                MessageBox.Show(connectionParams.ToString());
+                MessageBox.Show(connectionParams.getConnectionString());
+            }
+        }
 
-    private void btnSendMail_Click(object sender, EventArgs e)
-    {
-      msSendMail mailer = new msSendMail();
-      mailer.From = txtSendMailFrom.Text;
-      mailer.addTo(txtSendMailTo.Text);
-      mailer.Subject = txtSendMailSubject.Text;
-      mailer.Body = txtSendMailBody.Text;
-      mailer.smtpConfig.Host = txtSmtpHost.Text;
-      mailer.smtpConfig.Port = msConversion.stringToInt(txtSmtpPort.Text, 25);
-      mailer.smtpConfig.Username = txtSmtpUser.Text;
-      mailer.smtpConfig.Password = txtSmtpPassword.Text;
-      mailer.smtpConfig.EnableSSL = chkSmtpSSL.Checked;
+        private void btnSendMail_Click(object sender, EventArgs e)
+        {
+            msSendMail mailer = new msSendMail();
+            mailer.From = txtSendMailFrom.Text;
+            mailer.addTo(txtSendMailTo.Text);
+            mailer.Subject = txtSendMailSubject.Text;
+            mailer.Body = txtSendMailBody.Text;
+            mailer.smtpConfig.Host = txtSmtpHost.Text;
+            mailer.smtpConfig.Port = msConversion.stringToInt(txtSmtpPort.Text, 25);
+            mailer.smtpConfig.Username = txtSmtpUser.Text;
+            mailer.smtpConfig.Password = txtSmtpPassword.Text;
+            mailer.smtpConfig.EnableSSL = chkSmtpSSL.Checked;
 
-      string error = "";
-      if (mailer.Send(out error))
-      {
-        MessageBox.Show("messaggio inviato");
-      }
-      else
-      {
-        MessageBox.Show("ci sono errori: " + error);
-      }
-    }
+            string error = "";
+            if (mailer.Send(out error))
+            {
+                MessageBox.Show("messaggio inviato");
+            }
+            else
+            {
+                MessageBox.Show("ci sono errori: " + error);
+            }
+        }
 
-    private void button1_Click(object sender, EventArgs e)
-    {
-      var smtpConfigForm = new msSmtpConfigForm();
-      var smtpConfig = new msSmtpConfig();
-      if (smtpConfigForm.showDialog("msUtilities Test App", ref smtpConfig))
-      {
-        MessageBox.Show(smtpConfig.Name);
-        MessageBox.Show(smtpConfig.Host);
-      }
-    }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var smtpConfigForm = new msSmtpConfigForm();
+            var smtpConfig = new msSmtpConfig();
+            if (smtpConfigForm.showDialog("msUtilities Test App", ref smtpConfig))
+            {
+                MessageBox.Show(smtpConfig.Name);
+                MessageBox.Show(smtpConfig.Host);
+            }
+        }
 
-    private void buttonTestMessages_Click(object sender, EventArgs e)
-    {
-      // String message = String.Format(Messages.databaseConnectionError, "some host", "some database", "some error").Replace("\\n", Environment.NewLine);
-      String message = msText.newLine(String.Format(Messages.databaseConnectionError, "some host", "some database", "some error"));
-      MessageBox.Show(message);
-    }
+        private void buttonTestMessages_Click(object sender, EventArgs e)
+        {
+            // String message = String.Format(Messages.databaseConnectionError, "some host", "some database", "some error").Replace("\\n", Environment.NewLine);
+            String message = msText.newLine(String.Format(Messages.databaseConnectionError, "some host", "some database", "some error"));
+            MessageBox.Show(message);
+        }
 
-    private void btnTestMinutesToDateTime_Click(object sender, EventArgs e)
-    {
-      MessageBox.Show(msConversion.minutesToDateTime(1000).ToString());
-    }
+        private void btnTestMinutesToDateTime_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(msConversion.minutesToDateTime(1000).ToString());
+        }
 
-    private void txtStringToFloat_TextChanged(object sender, EventArgs e)
-    {
-      lblStringToFloat.Text = "String to Float: " + msConversion.stringToFloat((sender as TextBox).Text, 0).ToString();
+        private void txtStringToFloat_TextChanged(object sender, EventArgs e)
+        {
+            lblStringToFloat.Text = "String to Float: " + msConversion.stringToFloat((sender as TextBox).Text, 0).ToString();
+        }
     }
-  }
 }
---
+++ msUtilities_452_wpf_app/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using msUtilities.Database;
+using msUtilities.Database.Forms.Wpf;
+using System.Windows;
+
+namespace msUtilities_452_wpf_app
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConnectionForm_Click(object sender, RoutedEventArgs e)
+        {
+            var connectionParams = new msConnectionParams();
+            var connectionForm = new msConnectionFormWpf();
+            if (connectionForm.showDialog("wpf test", ref connectionParams))
+            {
+                MessageBox.Show(connectionParams.ToString());
+                MessageBox.Show(connectionParams.getConnectionString());
+            }
+        }
+
+        private void BtnConnectionFormSqlServer_Click(object sender, RoutedEventArgs e)
+        {
+            var connectionParams = new msConnectionParams();
+            connectionParams.databaseType = DatabaseType.SqlServer;
+            var connectionForm = new msConnectionFormWpf();
+            if (connectionForm.showDialog("wpf test", ref connectionParams))
+            {
+                MessageBox.Show(connectionParams.ToString());
+                MessageBox.Show(connectionParams.getConnectionString());
+            }
+        }
+    }
+}
