2725efced76cbb82dbb6343a0a88d41a8a4cc9ff
Phelipe de Sterlich <p.desterlich@gmail.com>
2016-07-18 09:00:12 +0200

    added project msUtilities.Database.Forms.Wpf.35
    dotNet 3.5 version for msUtilities.Database.Forms.Wpf

---
+++ msUtilities.Database.Forms.Wpf.35/msConnectionFormWpf.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows;
+
+namespace msUtilities.Database.Forms.Wpf
+{
+    /// <summary>
+    /// Logica di interazione per msConnectionFormWpf.xaml
+    /// </summary>
+    public partial class msConnectionFormWpf : Window
+    {
+        public bool showDialog(string title, ref msConnectionParams connectionParams)
+        {
+            Title = title;
+
+            BtnMostraPassword.Content = Messages.buttonShow;
+            BtnConfirm.Content = Messages.buttonOk;
+            BtnCancel.Content = Messages.buttonCancel;
+
+            LblType.Content = Messages.labelConnectionType;
+            LblHost.Content = Messages.labelConnectionHost;
+            LblDatabase.Content = Messages.labelConnectionDatabase;
+            LblUsername.Content = Messages.labelConnectionUsername;
+            LblPassword.Content = Messages.labelConnectionPassword;
+
+            CboType.SelectedIndex = CboType.Items.IndexOf(connectionParams.databaseType.ToString());
+            TxtHost.Text = connectionParams.host;
+            TxtDatabase.Text = connectionParams.database;
+            TxtUsername.Text = connectionParams.username;
+            TxtPassword.Password = connectionParams.password;
+
+            var dialog = ShowDialog();
+            if (dialog == null || !(bool)dialog) return false;
+
+            connectionParams.databaseType = (DatabaseType)Enum.Parse(typeof(DatabaseType), CboType.SelectedItem.ToString());
+            connectionParams.host = TxtHost.Text;
+            connectionParams.database = TxtDatabase.Text;
+            connectionParams.username = TxtUsername.Text;
+            connectionParams.password = TxtPassword.Password;
+
+            return true;
+        }
+
+        public msConnectionFormWpf()
+        {
+            InitializeComponent();
+
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            CboType.Items.Clear();
+            CboType.ItemsSource = Enum.GetNames(typeof(DatabaseType));
+        }
+
+        private void BtnConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void BtnMostraPassword_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(TxtPassword.Password, Title);
+        }
+    }
+}
