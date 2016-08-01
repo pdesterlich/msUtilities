b6e1463c57b1a164410fd04a7bf8995db5d6f50e
Phelipe de Sterlich <p.desterlich@gmail.com>
2016-08-01 11:29:23 +0200

    changed MsComparison methods to act as extension methods

--- msUtilities.452/msComparison.cs
+++ msUtilities.452/msComparison.cs
@@ -8,14 +8,18 @@ namespace msUtilities
     public static class MsComparison
     {
         /// <summary>
-        /// check if a DateTime value is in a range
+        /// extension method for DateTime to check if a DateTime value is in a range
         /// </summary>
         /// <param name="value">value to check</param>
         /// <param name="min">range lower limit</param>
         /// <param name="max">range upper limi</param>
         /// <param name="insideOnly">if true, value must not be equal to upper or lower limit (default false)</param>
         /// <returns>true if value is between limits, false otherwise</returns>
+#if NET20
         public static bool Between(DateTime value, DateTime min, DateTime max, bool insideOnly = false)
+#else
+        public static bool Between(this DateTime value, DateTime min, DateTime max, bool insideOnly = false)
+#endif
         {
             return insideOnly ? value > min && value < max : value >= min && value <= max;
         }
@@ -28,7 +32,11 @@ namespace msUtilities
         /// <param name="max">range upper limi</param>
         /// <param name="insideOnly">if true, value must not be equal to upper or lower limit (default false)</param>
         /// <returns>true if value is between limits, false otherwise</returns>
+#if NET20
         public static bool Between(int value, int min, int max, bool insideOnly = false)
+#else
+        public static bool Between(this int value, int min, int max, bool insideOnly = false)
+#endif
         {
             return insideOnly ? value > min && value < max : value >= min && value <= max;
         }
--- msUtilities_452_test/msComparisonTests.cs
+++ msUtilities_452_test/msComparisonTests.cs
@@ -12,11 +12,11 @@ namespace msUtilities.Tests
             var value = DateTime.Today;
             var lower = DateTime.Today.AddDays(-10);
             var upper = DateTime.Today.AddDays(10).Date;
-            var actual = MsComparison.Between(value, lower, upper);
+            var actual = value.Between(lower, upper);
             Assert.AreEqual(true, actual);
 
             value = DateTime.Today.AddDays(-20);
-            actual = MsComparison.Between(value, lower, upper);
+            actual = value.Between(lower, upper);
             Assert.AreEqual(false, actual);
         }
 
@@ -26,11 +26,11 @@ namespace msUtilities.Tests
             var value = 10;
             const int lower = 0;
             const int upper = 20;
-            var actual = MsComparison.Between(value, lower, upper);
+            var actual = value.Between(lower, upper);
             Assert.AreEqual(true, actual);
 
             value = -10;
-            actual = MsComparison.Between(value, lower, upper);
+            actual = value.Between(lower, upper);
             Assert.AreEqual(false, actual);
         }
     }
