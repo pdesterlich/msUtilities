de00eae1c56b2cd79a0430f3d6fc49075a1bf212
Phelipe de Sterlich <p.desterlich@gmail.com>
2016-08-02 17:32:27 +0200

    increased version to 1.3.37

--- msUtilities.35/Properties/AssemblyInfo.cs
+++ msUtilities.35/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build 
 // usando l'asterisco '*' come illustrato di seguito:
-// [assembly: AssemblyVersion("1.2.*")]
-[assembly: AssemblyVersion("1.2.36")]
-[assembly: AssemblyFileVersion("1.2.36")]
+// [assembly: AssemblyVersion("1.3.*")]
+[assembly: AssemblyVersion("1.3.37")]
+[assembly: AssemblyFileVersion("1.3.37")]
